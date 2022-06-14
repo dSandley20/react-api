f2737a4dc62e02496b90ae4d15abdeb378f5d06a
dSandley20 <danielsandley2@gmail.com>
2022-06-13 23:10:21 -0400

    get all and get single blog

--- Repositories/InMemBlogsRepository.cs
+++ Repositories/InMemBlogsRepository.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using react_api.Entities;
 using System;
+using System.Linq;
 
 namespace react_api.Repositories{
     public class InMemBlogsRepository{
@@ -8,6 +9,14 @@ namespace react_api.Repositories{
             new Blog{Id =  Guid.NewGuid(), Name = "First Blog" , Description = "This is my first blog", CreatedDate = DateTimeOffset.UtcNow },
             new Blog{Id =  Guid.NewGuid(), Name = "Second Blog" , Description = "This is my second blog", CreatedDate = DateTimeOffset.UtcNow },
             new Blog{Id =  Guid.NewGuid(), Name = "Third Blog" , Description = "This is my third blog", CreatedDate = DateTimeOffset.UtcNow },
+        };
+
+        public IEnumerable<Blog> GetBlogs(){
+            return blogs;
+        }
+
+        public Blog GetBlog(Guid id){
+            return blogs.Where(blog => blog.Id == id).SingleOrDefault();
         }
     }
 }
