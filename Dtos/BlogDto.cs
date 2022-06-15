b71b1e133bac17f44a560e69225b4fe25efd8b6b
dSandley20 <danielsandley2@gmail.com>
2022-06-14 22:59:35 -0400

    Create BlogDto to format blog data

---
+++ Dtos/BlogDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace react_api.Dtos
+{
+    public record BlogDto
+    {
+        public Guid Id { get; init; }
+        public string Name { get; init; }
+        public string Description { get; set; }
+        public DateTimeOffset CreatedDate { get; init; }
+    }
+}
