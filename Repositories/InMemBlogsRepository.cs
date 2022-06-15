62188576e5a0850a392ae999a1d84f4b3d72e25d
dSandley20 <danielsandley2@gmail.com>
2022-06-14 22:24:42 -0400

    extend interface to InMemBlogsRepository

--- Repositories/InMemBlogsRepository.cs
+++ Repositories/InMemBlogsRepository.cs
@@ -2,15 +2,18 @@ using System.Collections.Generic;
 using react_api.Entities;
 using System;
 using System.Linq;
+using react_api.RepositoryInterfaces;
 
 namespace react_api.Repositories{
-    public class InMemBlogsRepository{
+    public class InMemBlogsRepository : BlogsRepoInterface
+    
+    {
         private readonly List<Blog> blogs = new(){
             new Blog{Id =  Guid.NewGuid(), Name = "First Blog" , Description = "This is my first blog", CreatedDate = DateTimeOffset.UtcNow },
             new Blog{Id =  Guid.NewGuid(), Name = "Second Blog" , Description = "This is my second blog", CreatedDate = DateTimeOffset.UtcNow },
             new Blog{Id =  Guid.NewGuid(), Name = "Third Blog" , Description = "This is my third blog", CreatedDate = DateTimeOffset.UtcNow },
         };
-
+       
         public IEnumerable<Blog> GetBlogs(){
             return blogs;
         }
