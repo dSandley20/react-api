72e3561d94b50a390bd18fce6cc0dea09d5f5057
dSandley20 <danielsandley2@gmail.com>
2022-06-14 23:51:53 -0400

    Require Name and Description when making new blogs

---
+++ Dtos/CreateBlogDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace react_api.Dtos
+{
+    public record CreateBlogDto
+    {
+        [Required]
+        public string Name { get; init; }
+        [Required]
+        public string Description { get; init; }
+    }
+}
