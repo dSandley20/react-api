f96affe582f3b2f250ccd8369a9897c3b418e4b9
dSandley20 <danielsandley2@gmail.com>
2022-06-14 23:00:02 -0400

    Create utility function to convert blogs to BlogDtos

---
+++ Utilities/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using react_api.Dtos;
+using react_api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace react_api.Utilities
+{
+    public static class Extensions
+    {
+        public static BlogDto AsBlogDto(this Blog blog)
+        {
+            return new BlogDto { 
+                Id = blog.Id,
+                Name = blog.Name,
+                Description = blog.Description,
+                CreatedDate = blog.CreatedDate
+            };
+        }
+    }
+}
