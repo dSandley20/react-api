f9f5023ff9f14d77452e887f5b6fc632e306eb1f
dSandley20 <danielsandley2@gmail.com>
2022-06-14 22:23:39 -0400

    abstract InMemBlogs functions to an interface

---
+++ RepositoryInterfaces/BlogsRepoInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using react_api.Entities;
+
+namespace react_api.RepositoryInterfaces
+{
+   public  interface BlogsRepoInterface
+    {
+        public IEnumerable<Blog> GetBlogs();
+        public Blog GetBlog(Guid id);
+    }
+}
