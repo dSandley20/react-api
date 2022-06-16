232b7f8d3e4a88749b8702dfefe9e312bbd7173b
dSandley20 <danielsandley2@gmail.com>
2022-06-16 13:51:04 -0400

    create model for jobs

---
+++ Entities/Job.cs
@@ -0,0 +1,14 @@
+using System;
+namespace react_api
+{
+    public record Job{
+        public Guid Id {get; init;}
+        public string CompanyName {get; init;}
+        public string Position {get; init;}
+        public string Description {get; init;}
+        public DateTimeOffset startDate {get; init;}
+        public DateTimeOffset endDate {get; init;}
+        public DateTimeOffset CreatedDate {get; init;}
+        public DateTimeOffset? UpdatedDate {get; init;}
+    }
+}
