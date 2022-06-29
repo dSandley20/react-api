3208698aa3012a55f5186d07aa169e5cbf8946fd
dSandley20 <danielsandley2@gmail.com>
2022-06-29 11:10:38 -0400

    Add interfaces for References and Projects, basic CRUD operations

---
+++ RepositoryInterfaces/IProjectsRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using react_api.Entities;
+namespace react_api.RepositoryInterfaces
+{
+    public interface IProjectsRepository
+    {
+        public IEnumerable<Project> GetProjects();
+        public Project GetProject(Guid id);
+        public void CreateProject(Project project);
+        void UpdateProject(Project project);
+        void DeleteProject(Guid id);
+    }
+}
---
+++ RepositoryInterfaces/IRefrencesRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using react_api.Entities;
+
+namespace react_api.RepositoryInterfaces
+{
+    public interface IRefrencesRepository
+    {
+        public IEnumerable<Reference> GetRefrences();
+        public Reference GetReference(Guid id);
+        public void CreateReference(Reference reference);
+        void UpdateReference(Reference reference);
+        void DeleteReference(Guid id);
+    }
+}
