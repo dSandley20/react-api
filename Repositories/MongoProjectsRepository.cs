a8266c4346ce5208a15d915a780ed3b9112da60c
dSandley20 <danielsandley2@gmail.com>
2022-06-29 11:11:05 -0400

    Implement the Interfaces into the Repositories that the controllers will use

---
+++ Repositories/MongoProjectsRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using react_api.Entities;
+using react_api.RepositoryInterfaces;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace react_api.Repositories
+{
+    public class MongoProjectsRepository : IProjectsRepository
+    {
+        private readonly IMongoCollection<Project> projectCollection;
+        private readonly FilterDefinitionBuilder<Project> projectFilter = Builders<Project>.Filter;
+
+        public MongoProjectsRepository(IMongoDatabase client)
+        {
+            projectCollection = client.GetCollection<Project>("projects");
+        }
+
+        public void CreateProject(Project project)
+        {
+            projectCollection.InsertOne(project);
+        }
+
+        public void DeleteProject(Guid id)
+        {
+            projectCollection.DeleteOne(projectFilter.Eq(existingProject => existingProject.Id, id));
+        }
+
+        public Project GetProject(Guid id)
+        {
+            return projectCollection.Find(projectFilter.Eq(existingProject => existingProject.Id, id)).FirstOrDefault();
+        }
+
+        public IEnumerable<Project> GetProjects()
+        {
+            return projectCollection.Find(new BsonDocument()).ToList();
+        }
+
+        public void UpdateProject(Project project)
+        {
+            projectCollection.ReplaceOne(projectFilter.Eq(existingProject => existingProject.Id, project.Id), project);
+        }
+    }
+}
---
+++ Repositories/MongoReferencesRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using react_api.Entities;
+using react_api.RepositoryInterfaces;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using System.Collections.Generic;
+
+namespace react_api.Repositories
+{
+    public class MongoReferencesRepository : IRefrencesRepository
+    {
+        private readonly IMongoCollection<Reference> referenceCollection;
+        private readonly FilterDefinitionBuilder<Reference> referenceFilter = Builders<Reference>.Filter;
+
+        public MongoReferencesRepository(IMongoDatabase client)
+        {
+            referenceCollection = client.GetCollection<Reference>("references");
+        }
+
+        public void CreateReference(Reference reference)
+        {
+           referenceCollection.InsertOne(reference);
+        }
+
+        public void DeleteReference(Guid id)
+        {
+            referenceCollection.DeleteOne(referenceFilter.Eq(existingReference => existingReference.Id, id));
+        }
+
+        public Reference GetReference(Guid id)
+        {
+            return referenceCollection.Find(referenceFilter.Eq(existingReference => existingReference.Id, id)).FirstOrDefault();
+        }
+
+        public IEnumerable<Reference> GetRefrences()
+        {
+            return referenceCollection.Find(new BsonDocument()).ToList();
+        }
+
+        public void UpdateReference(Reference reference)
+        {
+            referenceCollection.ReplaceOne(referenceFilter.Eq(existingReference => existingReference.Id, reference.Id), reference);
+        }
+    }
+}
