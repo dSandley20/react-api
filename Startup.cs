c299af520f7f400467199e82c9f5d7d2720db5a0
dSandley20 <danielsandley2@gmail.com>
2022-06-29 11:09:19 -0400

    Add Projects and References Repositories to the startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,19 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using react_api.RepositoryInterfaces;
 using react_api.Repositories;
 using react_api.Entities;
+using react_api.Settings;
 using MongoDB.Driver;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
@@ -47,6 +41,9 @@ namespace react_api
 
             //registering all of the controllers for the API
             services.AddSingleton<IBlogsRepository, MongoBlogsRepository>();
+            services.AddSingleton<IJobsRepository, MongoJobsRepository>();
+            services.AddSingleton<IRefrencesRepository, MongoReferencesRepository>();
+            services.AddSingleton<IProjectsRepository, MongoProjectsRepository>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -61,8 +58,6 @@ namespace react_api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-           
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
