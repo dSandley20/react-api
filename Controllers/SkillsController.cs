01bd39e8932bb527e15cbf82e5a5fafd9f9b13ba
dSandley20 <danielsandley2@gmail.com>
2022-07-12 15:30:01 -0400

    open up the endpoints

---
+++ Controllers/SkillsController.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using react_api.Repositories;
+using react_api.RepositoryInterfaces;
+using react_api.Dtos.Skills;
+using react_api.Utilities;
+using react_api.Entities;
+
+namespace react_api.Controllers
+{
+    [ApiController]
+    [Route("/skills")]
+    public class SkillsController : ControllerBase
+    {
+        private readonly ISkillsRepository repository;
+
+        public SkillsController(ISkillsRepository repository){
+            this.repository = repository;
+        }
+
+        [HttpsGet]
+        public IEnumerable<SkillDto> GetSkills(){
+            return repository.GetSkills().Select(skill => skill.AsSkillDto());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<SkillDto> GetSkill(Guid id){
+            var skill = repository.GetSkill(id);
+            if(skill is null){
+                return NotFound();
+            }
+            return skill.AsSkillDto();
+        }
+
+        [HttpPost]
+        public ActionResult<SkillDto> CreateSkill(CreateUpdateSkillDto skillDto){
+            Skill skill = new Skill(){
+                Id = Guid.NewGuid(),
+                Name = skillDto.Name,
+                Description = skillDto.Description,
+                YearsExperience = skillDto.YearsExperience,
+                Image = skillDto.Image,
+                Project = skillDto.Project
+            };
+
+            repository.CreateSkill(skill);
+            return CreatedAtAction(nameof(GetSkill), new {id = skill.Id} , skill.AsSkillDto());
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<SkillDto> UpdateSkill(Guid id, CreateUpdateSkillDto skillDto){
+            var existingSkill = repository.GetSkill(id);
+            if(existingSkill is null){
+                return NotFound();
+            }
+            var skill = existingSkill with {
+                Name = skillDto.Name,
+                Description = skillDto.Description,
+                YearsExperience = skillDto.YearsExperience,
+                Image = skillDto.Image,
+                Project = skillDto.Project
+            };
+            repository.UpdateSkill(skill);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<SkillDto> DeleteSkill(Guid id){
+            var existingSkill = repository.GetSkill(id)
+            if(existingSkill is null){
+                return NotFound();
+            }
+            repository.DeleteSkill(id);
+            return NoContent();
+        }
+    }
+}
